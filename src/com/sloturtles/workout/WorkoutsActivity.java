ad0df45d2d7c82d2084184c3652afca3794093b5
Jon Amireh <jon.amireh@gmail.com>
2012-11-09 11:31:14 -0800

    Working test Tabs

--- src/com/sloturtles/workout/WorkoutsActivity.java
+++ src/com/sloturtles/workout/WorkoutsActivity.java
@@ -3,6 +3,8 @@ package com.sloturtles.workout;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class WorkoutsActivity extends Activity {
 
@@ -10,6 +12,22 @@ public class WorkoutsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts);
+        
+        TabHost th = (TabHost)findViewById(R.id.tabhost);
+        th.setup();
+        TabSpec specs;
+        specs = th.newTabSpec("tag1");
+        specs.setContent(R.id.Favorites);
+        specs.setIndicator("Favorites");
+        th.addTab(specs);
+        specs = th.newTabSpec("tag2");
+        specs.setContent(R.id.Workouts);
+        specs.setIndicator("Workouts");
+        th.addTab(specs);
+        specs = th.newTabSpec("tag3");
+        specs.setContent(R.id.Progress);
+        specs.setIndicator("Progress");
+        th.addTab(specs);
     }
 
     @Override
